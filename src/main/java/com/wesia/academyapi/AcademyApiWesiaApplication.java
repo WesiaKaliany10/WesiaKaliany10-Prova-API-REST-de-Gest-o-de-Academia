954859758b861f5051ffcae9d9ac0e967da22b40
Wésia Kaliany Lima Peixoto <161386123+WesiaKaliany10@users.noreply.github.com>
2025-10-06 23:33:33 -0300

    Update AcademyApiWesiaApplication.java

--- src/main/java/com/wesia/academyapi/AcademyApiWesiaApplication.java
+++ src/main/java/com/wesia/academyapi/AcademyApiWesiaApplication.java
@@ -1,11 +1,1 @@
-package com.wesia.academyapi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AcademyApiWesiaApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AcademyApiWesiaApplication.class, args);
-    }
-}
